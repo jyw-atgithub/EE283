6edb38674794897f498616a301a1bc1cce38942c
jyw-atgithub <jenyuw@uci.edu>
2023-02-17 16:44:29 -0800

    finished

--- week_5/hw5_5.sh
+++ week_5/hw5_5.sh
@@ -11,11 +11,15 @@ module load vcftools/0.1.16
 
 ##extract the first 1M of chromosome X
 
+vcftools --chr X --from-bp 1 --to-bp 1000000 --out ${dest}/out --012 --gzvcf ${SNP}/all_variants_filtered.vcf.gz 
+
+
+## another way to extract
+: <<'SKIP'
 bcftools view  -O v --threads 4 ${SNP}/all_variants_filtered.vcf.gz X:1-1000000 |\
 bcftools query -f '[%GT] \n' |\
 tr "|" "\/" | sed -r 's/(.{3})/\1\t/g' > ${dest}/genotype.txt
+SKIP
 
 
-vcftools --chr X --from-bp 1 --to-bp 1000000 --out out --012 --gzvcf ${SNP}/all_variants_filtered.vcf.gz 
-
-#cut -f 10,11,12,13
+#bcftools view -H -O v --threads 4 ${SNP}/all_variants_filtered.vcf.gz X:1-1000000 | cut -f 10,11,12,13 |less
